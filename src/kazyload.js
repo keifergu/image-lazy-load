eb4f2a088619dc74c9fb76c487e41dd62bd81cfb
Keifer Gu <keifergu@users.noreply.github.com>
2017-10-17 21:09:04 +0800

    feat(lazyload): Complete the basic lazyload feature.

--- src/kazyload.js
+++ src/kazyload.js
@@ -1,11 +1,25 @@
+function observeRunner(entries) {
+  entries.forEach(entry => {
+    if(entry.intersectionRatio > 0) {
+      var target = entry.target
+      var dataSrc = target.attributes.getNamedItem('data-src').value
+      target.src = dataSrc
+    }
+  })
+}
+
 class KazyLoad {
   constructor(query, settings) {
     this.elements = document.querySelectorAll(query)
     this.settings = Object.assign({}, settings)
-    this.observer = null
+    this.observer = new IntersectionObserver(observeRunner)
+
+    this._initObserver()
   }
 
   _initObserver() {
-        
+    this.elements.forEach(el => {
+      this.observer.observe(el)
+    })
   }
 }
